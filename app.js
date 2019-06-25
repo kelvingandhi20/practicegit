e8844e2419710aad0b5cb7502cbbb7ebdc325a77
kelvingandhi20 <ksg27@njit.edu>
2019-06-25 15:02:20 -0400

    added feature 5

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ This is test java script
 NEw changes has been made
 This is new line of Code. Enrique code.
 This is adding line in feature 2 branch
+
+Now I am adding feature 5 branch
+another change
