b51646876aa510a844faa5e3dec3a24b0d73471b
ecastelan310 <ecastelan310@gmail.com>
2019-06-25 12:32:48 -0400

    adds error handling by adding and deleting lines in feature4

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 This is test java script 
-NEw changes has been made
+Newer changes have been made in feature 4 (deleted then added this one in ft 4)
 This is new line of Code. Enrique code.
-This is adding line in feature 2 branch
+This is adding line in feature 4 branch (changed in ft 4)
+NOT feature2 (added in ft 4)
+sorry Kelvin (added in ft 4)
