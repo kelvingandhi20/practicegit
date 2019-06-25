7dcc878b7d5fcc3d4326d3cbd7d8d50661377a4b
TheMightyE <sk2293@njit.edu>
2019-06-25 12:34:32 -0400

    Adds feature3

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ This is test java script
 NEw changes has been made
 This is new line of Code. Enrique code.
 This is adding line in feature 2 branch
+This is Emad adding a line in feature3 branch.
+Another feature3 line.
+One more feature3 line.
